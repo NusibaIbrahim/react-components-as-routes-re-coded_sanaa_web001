4f0244dc5d8e2785cd20c02af97617936c6dc8f1
Nusiba Ibrahim <ainusiba@gmail.com>
2020-09-17 07:24:11 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Home = () => {
-  return (
+import Home from './Home'
+import About from './About'
+import Login from './Login'
+import Navbar from './Navbar'
+// Step 1. Import react-router functions
+/* Add NavLink to importer */
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+// Step 2. Changed to have router coordinate what is displayed
+ReactDOM.render((
+  <Router>
     <div>
       <h1>Home!</h1>
     </div>
